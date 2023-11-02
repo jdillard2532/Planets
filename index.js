2c1ac7714d3cedb9374923bcc64042c792660232
James Dillard <jamesdillard@Jamess-MacBook-Air.local>
2023-11-02 18:45:23 -0400

    update

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const data = require("./data.json");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("assets"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { data: data[0] });
+});
+app.get("/mercury", (req, res) => {
+  res.render("index.ejs", { data: data[0] });
+});
+
+app.get("/venus", (req, res) => {
+  res.render("index.ejs", { data: data[1] });
+});
+app.get("/earth", (req, res) => {
+  res.render("index.ejs", { data: data[2] });
+});
+app.get("/mars", (req, res) => {
+  res.render("index.ejs", { data: data[3] });
+});
+app.get("/jupiter", (req, res) => {
+  res.render("index.ejs", { data: data[4] });
+});
+app.get("/saturn", (req, res) => {
+  res.render("index.ejs", { data: data[5] });
+});
+app.get("/uranus", (req, res) => {
+  res.render("index.ejs", { data: data[6] });
+});
+app.get("/neptune", (req, res) => {
+  res.render("index.ejs", { data: data[7] });
+});
+
+app.listen(3000, () => {
+  console.log("Listen on port 3000");
+});
